a0864f5c4962bc413b46f69cde94463aa9c9e0fb
snkinard <snkinard@gmail.com>
2016-03-31 10:59:05 -0500

    mentions mentions mentions mentions

--- parser.py
+++ parser.py
@@ -1,8 +1,25 @@
 import json
+import re
+from sets import Set
 
 class Parser(object):
 
-    @staticmethod
-    def parse_message(msg):
-        contents = {"raw": msg}
-        return json.dumps(contents)
+    def __init__(self):
+        self.contents = {}
+
+    def parse_message(self, msg):
+        self.contents = {"raw": msg}
+        self.contents["mentions"] = self.extract_mentions(msg)
+
+    def to_json_str(self):
+        return json.dumps(self.contents)
+
+    def extract_mentions(self, msg):
+        rgx = r"\s@[a-zA-Z]+|\A@[a-zA-Z]+"
+        found = re.findall(rgx, msg)
+        mentions = Set()
+        # set to remove dupes
+        for mention in found:
+            # remove whitespace and @ character
+            mentions.add(mention.strip().replace("@", ""))
+        return list(mentions)
--- test_parser.py
+++ test_parser.py
@@ -1,4 +1,17 @@
 from parser import Parser
+import json
 
 def test_parser():
-    assert '{"raw": "I am Sam"}' == Parser.parse_message("I am Sam")
+    parse = Parser()
+    parse.parse_message("I am @sam and here is a link: https://www.simple.com have a nice day (smiley).")
+    expected = {"raw": "I am @sam and here is a link: https://www.simple.com have a nice day (smiley).", "mentions": ["sam"]}
+    assert json.dumps(expected) == parse.to_json_str()
+
+def test_extract_mentions():
+    parse = Parser()
+    msg = "@sam or hello @sam's fiancee is your name @katie? Or is it @katherine or whatever? is your email snkinard@gmail.com? also here is this thing @ "
+    mentions = parse.extract_mentions(msg)
+    assert 3 == len(mentions)
+    assert "sam" in mentions
+    assert "katie" in mentions
+    assert "katherine" in mentions
